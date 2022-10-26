301826b0922ebbc62988d200e364b2bc4d270078
abel mesfin <abelmesfin555@gmail.com>
2022-10-26 13:17:28 +0300

    Store first object inital commit

--- models/base_model.py
+++ models/base_model.py
@@ -4,13 +4,15 @@ import uuid
 from datetime import datetime
 
 
-class BaseModel():
+class BaseModel:
     """initilixing base model."""
     def __init__(self, *args, **kwargs):
         if not kwargs:
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
@@ -20,17 +22,18 @@ class BaseModel():
 
     def save(self):
         """saves """
+        from models import storage
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        return {
-                "name": self.name,
-                "my_number": self.my_number,
-                "id": self.id,
-                "__class__": type(self).__name__,
-                "created_at": self.created_at.isoformat(),
-                "updated_at": self.updated_at.isoformat()
-                }
+        dictionary = {}
+        dictionary.update(self.__dict__)
+
+        dictionary.update({"__class__": type(self).__name__})
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
 
     def __str__(self):
         return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
