e7517e171f8c7d1f9d8101508ee6cf9d1bb3fab5
abel mesfin <abelmesfin555@gmail.com>
2022-10-28 15:06:44 +0300

    pycodestyle fixed

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ from datetime import datetime
 class BaseModel:
     """initilixing base model."""
     def __init__(self, *args, **kwargs):
+        """Base model instance initilizition."""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
@@ -15,18 +16,21 @@ class BaseModel:
             storage.new(self)
 
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def save(self):
-        """saves """
+        """saves file in to file.json file and update update time"""
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
+        """ Convert an instance in to modified dictionary format"""
         dictionary = {}
         dictionary.update(self.__dict__)
 
@@ -36,5 +40,5 @@ class BaseModel:
         return dictionary
 
     def __str__(self):
-        return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
-
+        return ("[{}] ({}) {}".
+                format(type(self).__name__, self.id, self.__dict__))
