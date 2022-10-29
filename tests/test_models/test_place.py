59f74bc874536b8be1760025577ef498646fddef
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-10-28 22:11:06 -0700

    place model  test

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,65 +1,68 @@
 #!/usr/bin/python3
-"""..."""
-from tests.test_models.test_base_model import test_basemodel
-from models.place import Place
+"""
+This module is the unittest file for the class: Place.
+"""
+from genericpath import exists
 import unittest
+from models.place import Place
+from models.engine.file_storage import FileStorage
+import pep8
+from models import storage
 
 
-class test_Place(unittest.TestCase):
-    """..."""
+class TestBaseClass(unittest.TestCase):
+    """
+    This class is for testing Place.
+    """
+    def setUp(self):
+        """
+        Setup method.
+        """
+        self.Place1 = Place()
+        self.Place2 = Place()
+        Place3_dict = self.Place1.to_dict()
+        self.Place3 = Place(**Place3_dict)
 
-    def __init__(self, *args, **kwargs):
-        """..."""
-        super().__init__(*args, **kwargs)
-        self.name = 'Place'
-        self.value = Place
+    def tearDown(self):
+        """
+        Teardown method.
+        """
+        del self.Place1
+        del self.Place2
+        del self.Place3
+        storage.save()
 
-    def test_place_cityid(self):
-        """..."""
-        cls = self.value()
-        self.assertEqual(type(cls.city_id), str)
+    def test_pep8(self):
+        """
+        Testing pep8 compliance.
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_place_userid(self):
-        """..."""
-        cls = self.value()
-        self.assertEqual(type(cls.user_id), str)
+    def test_documentation(self):
+        """
+        tests for module, class, & method documentation.
+        """
+        # Class docstring
+        self.assertTrue(len(Place.__doc__) >= 1)
 
-    def test_place_name(self):
-        """..."""
-        cls = self.value()
-        self.assertEqual(type(cls.name), str)
+    def test_init(self):
+        """
+        Tests the init method.
+        """
+        self.assertEqual(self.Place1.city_id, "")
+        self.assertEqual(self.Place1.user_id, "")
+        self.assertEqual(self.Place1.name, "")
+        self.assertEqual(self.Place1.description, "")
+        self.assertEqual(self.Place1.number_rooms, 0)
+        self.assertEqual(self.Place1.number_bathrooms, 0)
+        self.assertEqual(self.Place1.max_guest, 0)
+        self.assertEqual(self.Place1.price_by_night, 0)
+        self.assertEqual(self.Place1.latitude, 0.0)
+        self.assertEqual(self.Place1.longitude, 0.0)
+        self.assertEqual(self.Place1.amenity_ids, [])
 
-    def test_place_description(self):
-        """..."""
-        cls = self.value()
-        self.assertEqual(type(cls.description), str)
-
-    def test_place_numberrooms(self):
-        """..."""
-        cls = self.value()
-        self.assertEqual(type(cls.number_rooms), int)
-
-    def test_place_numberBathRooms(self):
-        """..."""
-        cls = self.value()
-        self.assertEqual(type(cls.number_bathrooms), int)
-
-    def test_place_maxGuest(self):
-        """..."""
-        cls = self.value()
-        self.assertEqual(type(cls.max_guest), int)
-
-    def test_place_priceByNight(self):
-        """..."""
-        cls = self.value()
-        self.assertEqual(type(cls.price_by_night), int)
-
-    def test_place_latitude(self):
-        """..."""
-        cls = self.value()
-        self.assertEqual(type(cls.latitude), float)
-
-    def test_place_amenityIds(self):
-        """..."""
-        cls = self.value()
-        self.assertEqual(type(cls.amenity_ids), list)
+if __name__ == "__main__":
+    unittest.main()
